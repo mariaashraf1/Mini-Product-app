ded4a2a9964db19824fa39149f85d4cc97135cb9
mariaashraf1 <115638891+mariaashraf1@users.noreply.github.com>
2023-04-24 20:48:15 +0200

    Add files via upload

---
+++ lib/product.dart
@@ -0,0 +1,16 @@
+class Product
+{
+String imageURL;
+String name;
+double price;
+
+
+Product({required this.name,   required this.imageURL, required this.price});
+}
+
+Product sampleProduct1 = Product(name: 'Earings 1',     imageURL: "asset/iamge 5.png", price: 50);
+Product sampleProduct2 = Product(name: 'Earings 2',   imageURL: "asset/image1.jpg", price: 55);
+Product sampleProduct3 = Product(name: 'Earings 3',   imageURL: "asset/image2.jpg", price: 100);
+Product sampleProduct4 = Product(name: 'Earings 4',    imageURL: "asset/image 3.jpg", price: 70);
+Product sampleProduct5 = Product(name: 'Earings 5',  imageURL: "asset/image 4.jpg", price: 80);
+
